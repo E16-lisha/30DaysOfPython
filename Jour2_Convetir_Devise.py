b7347bbaa301d5fe9721643fe3e97185018c24d3
Elisée N'tsoukou <eliseentsoukou@gmail.com>
2025-07-26 23:01:41 +0000

    Jour 5. Ce programme calcule ton indice de Masse corporelle (IMC) et te retoure ton état de santé ainsi que quelques recommendations.

--- Jour2_Convetir_Devise.py
+++ Jour2_Convetir_Devise.py
@@ -3,13 +3,14 @@ Nom du fichier : #Convertisseur_CFAEURODOLLAR.py
 Projet : Concvertiseur de devise CFA <---> DOLLAR/EURO
 Auteur : Elisée N'TSOUKOU
 Date :  23/07/2025
-Version : 1.0
+Version : 1.1
 Communauté : PyCon Togo ( 30 Days Pycon)
-Dernière Mise à jour : 24 Juillet 2025
+Dernière Mise à jour : 26/07/2025
 
 '''
-
+print("Bientôt le système vous permettra de faires des conversions FCFA vres EURO ou $ et vice verca ")
+print("")
 devise = int(input("Entrez la valeur du FCFA : "))
 convt1 = devise/656
 convt2 = devise/559.4
-print(f"{devise} FCFA équicaut à {convt1:.2f}EUR et {convt2:.2f}$ USD.")
+print(f"{devise} FCFA équivaut à {convt1:.2f}EUR et {convt2:.2f}$ USD.")
