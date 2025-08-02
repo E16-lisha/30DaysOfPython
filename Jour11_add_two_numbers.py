5f60be3741892fa610f04dbbe3bc7f1f869a0ea8
Elisée N'tsoukou <eliseentsoukou@gmail.com>
2025-08-02 23:54:28 +0000

    Jour 11. Ce programme Utilise une fonction pour calculer la somme de deux nombres

--- Jour11_add_two_numbers.py
+++ Jour11_add_two_numbers.py
@@ -3,9 +3,10 @@ Nom du fichier : Jour11_add_two_numbers.py
 Projet : Addition de deux nombres avec fonction
 Objectif : Apprendre à créer une fonction avec des paramètres
 Auteur : Elisée N'TSOUKOU
-Date : 01/08/2025
+Date : 02/08/2025
 Version : 1.0
 Communauté : PyCon Togo (30 Days PyCon)
+Dernière Mise à jour :
 '''
 
 def add_two_numbers(nombre1, nombre2):
