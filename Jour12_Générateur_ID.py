74fa554c3091e9fac88daad4622705ab57a5a8b1
Elisée N'tsoukou <eliseentsoukou@gmail.com>
2025-08-03 23:32:38 +0000

    Jour 12. Ce programme Utilise la fonction aléatoire de python pour générer les identifiants utilisateurs

--- Jour12_Générateur_ID.py
+++ Jour12_Générateur_ID.py
@@ -1,10 +1,26 @@
 '''
-Nom du fichier : Jour11_add_two_numbers.py
-Projet : Addition de deux nombres avec fonction
-Objectif : Apprendre à créer une fonction avec des paramètres
+Nom du fichier : Jour12_Générateur_ID.py
+Projet : Générateur d’identifiant utilisateur aléatoire
+Objectif : Utiliser les modules et les fonctions
 Auteur : Elisée N'TSOUKOU
-Date : 02/08/2025
+Date : 03/08/2025
 Version : 1.0
 Communauté : PyCon Togo (30 Days PyCon)
 Dernière Mise à jour :
 '''
+
+import random
+import string
+
+# Déclaration de la fonction pour générer un ID aléatoire
+def random_user_id():
+    caracteres = string.ascii_letters + string.digits  # Lettres A-Z, a-z et chiffres 0-9
+    ID = ''.join(random.choice(caracteres) for i in range(6))  # 6 caractères aléatoires
+    return ID
+
+
+print("="*60)
+print("Générateur d'ID utilisateur aléatoire (6 caractères)")
+print("="*60)
+print(f"Voici ton ID généré : {random_user_id()}")
+print("="*60)
---
+++ Jour13_List_Comprehension.py
@@ -0,0 +1,10 @@
+'''
+Nom du fichier : Jour11_add_two_numbers.py
+Projet : Addition de deux nombres avec fonction
+Objectif : Apprendre à créer une fonction avec des paramètres
+Auteur : Elisée N'TSOUKOU
+Date : 02/08/2025
+Version : 1.0
+Communauté : PyCon Togo (30 Days PyCon)
+Dernière Mise à jour :
+'''
