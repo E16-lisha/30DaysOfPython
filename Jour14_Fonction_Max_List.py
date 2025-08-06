427c44050579bf7cf251b854c636f65e0f3dae7a
Elisée N'tsoukou <eliseentsoukou@gmail.com>
2025-08-06 00:11:18 +0000

    Jour 14. Ce programme créé Une fonction qui retourne l'elément maximal d'une liste

--- Jour14_Fonction_Max_List.py
+++ Jour14_Fonction_Max_List.py
@@ -20,6 +20,7 @@ def max_liste(liste) :
 
 #Programme principal
 print("Salut ! Heureux de vous retrouver...")
+print("")
 print("Le travail d'aujourd'hui ressemble un peu à celui fait hier. Nous allons juste rechercher le maximun d'une liste.")
 taille = int(input("Combien d’éléments voulez-vous dans votre liste ? : "))
 liste = []
