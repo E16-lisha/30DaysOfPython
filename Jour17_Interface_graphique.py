752cff5f656af2b8d93996aec45d59a18bac93d1
Elisée N'tsoukou <eliseentsoukou@gmail.com>
2025-08-08 18:24:03 +0000

    Jour 17. J'ai codé mon interface graphique en Python.

---
+++ Jour17_Interface_graphique.py
@@ -0,0 +1,18 @@
+'''
+Nom du fichier : Jour17_Interface_graphique.py
+Projet : Création d'interface graphique
+Objectif : se familiariser avec Tkinter
+Auteur : Elisée N'TSOUKOU
+Date : 08/08/2025
+Version : 1.0
+Communauté : PyCon Togo (30 Days PyCon)
+Dernière Mise à jour :
+'''
+from tkinter import *
+window = Tk()
+window.title("WSINDE Pressing")
+window.config(background='#005490')
+window.iconbitmap("logo.ico")
+print("Hello World")
+window.mainloop()
+
